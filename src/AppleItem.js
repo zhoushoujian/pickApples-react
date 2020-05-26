63b24ae2d01e47ddee3e5a908a1e83a11d1241f1
shoujian <shoujian.zhou@shuyun.com>
2020-05-26 12:49:35 +0800

    update demo

--- src/AppleItem.js
+++ src/AppleItem.js
@@ -1,37 +1,37 @@
-import React , {Component} from 'react';
+import React from 'react';
 
-class AppleItem extends Component {
+const AppleItem = ({
+  changeWeight,
+  eat,
+  apple,
+  index,
+}) => {
 
-  changeWeight = () => {
-    this.props.changeWeight(this.props.apple.weight)
+  const changeAppleWeight = () => {
+    changeWeight(apple.weight)
   }
 
-  eat = () => {
-    this.props.eat(this.props.index); //将参数也一起传到父组件
-  }
+  const eatApple = () => eat(index)
 
-  render() {
-    let {apple} = this.props;
-    return (
-      <div className="appleItem">
-        <div className="apple">
-          <img src="./apple.png" alt="apple" / >
+  return (
+    <div className="appleItem">
+      <div className="apple">
+        <img src="./apple.png" alt="apple" />
+      </div>
+      <div className="info">
+        <div className="name">
+            {apple.name}{apple.num}号
         </div>
-        <div className="info">
-          <div className="name">
-              {apple.name}{apple.num}号
-          </div>
-          <div className="weight">
-            {apple.weight}克
-          </div>
+        <div className="weight">
+          {apple.weight}克
         </div>
-        <div className="btn-div">
-          <button onClick={this.eat}>吃掉</button>
-          <button onClick={this.changeWeight}>重量改变</button>   {/* 调用方法,将子组件方法传递到父组件 */}
-        </div>
-      </div> 
-    )
-  }
+      </div>
+      <div className="btn-div">
+        <button onClick={eatApple}>吃掉</button>
+        <button onClick={changeAppleWeight}>重量改变</button>   {/* 调用方法,将子组件方法传递到父组件 */}
+      </div>
+    </div> 
+  )
 }
 
 export default AppleItem;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';  //这个模块不用看，仅为了支撑服务
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
